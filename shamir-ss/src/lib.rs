65c699106a739e3d8f1df8dde0b086c5bb6fea72
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-04-02 00:37:58 +0200

    Implement polynomial evaluation and interpolation

--- shamir-ss/src/lib.rs
+++ shamir-ss/src/lib.rs
@@ -1,35 +1,138 @@
+use std::iter::repeat_with;
+
 // use ark_poly::domain::radix2::Radix2EvaluationDomain;
+use ark_ff::fields::Field;
 use ark_bn254::Fr;
 
-/// Takes k values, encodes them as 2k values.
-pub fn encode(value: &Vec<Fr>) -> Vec<Fr> {
-    unimplemented!()
+/// Defines a set of points to evaluate the polynomial in.
+pub struct Domain {
+    g: Fr,
+    k: usize,
+    degrees: Vec<Fr>,
 }
 
-/// Takes 2k values, some (but no more than k) of which are unknown/lost, and
-/// decodes back the k. This inverses the result of encode:
-///
-/// ```ignore
-/// let vs = vec![1, 2, 3];
-/// let code = encode(&vs);
-/// assert_eq!(6, code.len());
-/// let code = code.map(|x| Some(x)).collect();
-/// assert_eq!(vs, decode(&code));
-/// ```
-///
-/// even if some values are lost
-///
-/// ```ignore
-/// let vs = vec![1, 2, 3];
-/// let code = encode(&vs);
-/// let mut code = code.map(|x| Some(x)).collect();
-/// code[1] = None;
-/// code[4] = None;
-/// code[5] = None;
-/// assert_eq!(vs, decode(&code));
-/// ```
-pub fn decode(code: &Vec<Option<Fr>>) -> Vec<Fr> {
-  unimplemented!()
+impl Domain {
+
+    /// `g` is the generator of the domain group <g>, k defines the number of
+    /// elements 2^k to evaluate the polynomial in.
+    pub fn new(g: Fr, k: usize) -> Self {
+        let mut acc = g.clone();
+        let degrees = repeat_with(|| {
+            acc *= g;
+            acc.clone()
+        }).take(1 << (k + 1)).collect();
+        Domain { g, k, degrees }
+    }
+
+    /// Takes a list of (x, y) points, produces the list of polynomial's
+    /// coefficients
+    fn interpolate(ps: &Vec<(Fr, Fr)>, xs: &Vec<Fr>) -> Vec<Fr> {
+        // computes δ_j(x)
+        let delta = |j: usize, x: &Fr| -> Fr {
+            let (x_j, y_j) = ps[j];
+            ps.iter().enumerate().map(|(m, (x_m, y_m))| {
+                if m != j {
+                    (x - x_m) / (x_j - x_m)
+                } else {
+                    Fr::ONE
+                }
+            }).product()
+        };
+        xs.iter().map(|x| {
+            ps.iter().enumerate().map(|(m, (_, y_m))| {
+                delta(m, x) * y_m
+            }).sum()
+        }).collect()
+    }
+
+    /// Takes 2^k values, encodes them as 2^(1+k) values.
+    ///
+    /// The original values can be found inside the codeword on even
+    /// positions. The odd positions are filled with the other correlated data
+    /// (polynomial evaluations that serve as "checksums"):
+    ///
+    /// ```
+    /// use shamir_ss::Domain;
+    /// use ark_ff::fields::Field;
+    /// use ark_bn254::Fr;
+    /// let d = Domain::new(Fr::from(2), 2);
+    /// let v : Vec<Fr> = vec![1, 2, 3, 4].iter().map(|&x| Fr::from(x)).collect();
+    /// let c = d.encode(v.clone());
+    /// assert_eq!(v, vec![c[0], c[2], c[4], c[6]]);
+    /// ```
+    pub fn encode(&self, value: Vec<Fr>) -> Vec<Fr> {
+        let even = (0..).step_by(2).take(1 << self.k);
+        let odd  = (1..).step_by(2).take(1 << self.k);
+
+        let known = even.zip(value.iter()).map(|(i, y_i)| {
+            (self.degrees[i], y_i.clone())
+        }).collect();
+        let wanted = odd.map(|i| self.degrees[i]).collect();
+        let extra = Self::interpolate(&known, &wanted);
+        let mut res = vec![];
+        // intersperse even and odd
+        for (e, o) in value.into_iter().zip(extra.into_iter()) {
+            res.push(e);
+            res.push(o);
+        }
+        res
+    }
+
+    /// Takes 2k values, some (but no more than k) of which are unknown/lost, and
+    /// decodes back the k. This inverses the result of encode:
+    ///
+    /// ```
+    /// use shamir_ss::Domain;
+    /// use ark_ff::fields::Field;
+    /// use ark_bn254::Fr;
+    /// let d = Domain::new(Fr::from(2), 2);
+    /// let v : Vec<Fr> = vec![1, 2, 3, 4].iter().map(|&x| Fr::from(x)).collect();
+    /// let c = d.encode(v.clone()).into_iter().map(|x| Some(x)).collect();
+    /// assert_eq!(Some(v), d.decode(&c));
+    /// ```
+    ///
+    /// even if some values are lost
+    ///
+    /// ```
+    /// use shamir_ss::Domain;
+    /// use ark_ff::fields::Field;
+    /// use ark_bn254::Fr;
+    /// let d = Domain::new(Fr::from(2), 2);
+    /// let v : Vec<Fr> = vec![1, 2, 3, 4].iter().map(|&x| Fr::from(x)).collect();
+    /// let mut c : Vec<Option<Fr>> = d.encode(v.clone()).into_iter().map(|x| Some(x)).collect();
+    /// c[0] = None;
+    /// c[1] = None;
+    /// c[4] = None;
+    /// assert_eq!(Some(v), d.decode(&c));
+    /// ```
+    ///
+    /// but if you erase more than half of the points, the decoding will fail:
+    ///
+    /// ```
+    /// use shamir_ss::Domain;
+    /// use ark_ff::fields::Field;
+    /// use ark_bn254::Fr;
+    /// let d = Domain::new(Fr::from(2), 2);
+    /// let v : Vec<Fr> = vec![1, 2, 3, 4].iter().map(|&x| Fr::from(x)).collect();
+    /// let mut c : Vec<Option<Fr>> = d.encode(v.clone()).into_iter().map(|x| Some(x)).collect();
+    /// c[0] = None;
+    /// c[1] = None;
+    /// c[3] = None;
+    /// c[4] = None;
+    /// assert_eq!(Some(v), d.decode(&c));
+    /// ```
+    pub fn decode(&self, code: &Vec<Option<Fr>>) -> Option<Vec<Fr>> {
+        let known : Vec<_> = self.degrees.iter().zip(code).flat_map(|(x, y)| {
+            y.map(|y| (x.clone(), y))
+        }).collect();
+        let wanted = self.degrees.iter().cloned().step_by(2).take(1 << self.k).collect();
+        if known.len() >= (1 << self.k) {
+            Some(Self::interpolate(&known, &wanted))
+        } else {
+            None
+        }
+    }
+
 }
 
 #[cfg(test)]
