9c6eba08ba8711aa89366628686bf9ccbc904edc
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-04-01 15:18:17 +0200

    Add an empty directory for fft implementation

---
+++ shamir-ss/src/lib.rs
@@ -0,0 +1,16 @@
+use ark_poly::domain::radix2::Radix2EvaluationDomain;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
